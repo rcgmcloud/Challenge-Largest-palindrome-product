3af62dc495860918cae5d188ecd6a4c25559cdaa
Raymond Mariano <rcm@hawaii.edu>
2015-05-23 11:39:59 -1000

    FINISHED

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -8,12 +8,31 @@
  *  and the palindromeNumber itself.
  */
 module.exports = function(multiplicands, digits){
+  var palindromeNumber = 0;
+  var numString = [];
+  for(var i=0; i<digits; i++){
+    numString.push(9);
+  }
+
+  var factorTest_0 = Number(numString.join(""));
+  var factorTest_1 = Number(numString.join(""));
+  var set = factorTest_0;
   var factor_0 = 0;
   var factor_1 = 0;
-  var palindromeNumber = 0;
-  // do your work here
 
-
+  for(var j = set; j>1; j--){
+    factorTest_1 = j;
+    for(var i = set; i>1; i--){
+      factorTest_0 = i;
+      var numTest = factorTest_1 * factorTest_0;
+      var numTestReverse = Number(numTest.toString().split("").reverse().join(""));
+      if(numTest === numTestReverse && numTest > palindromeNumber){
+          palindromeNumber = numTest;
+          factor_0 = i;
+          factor_1 = j;
+      }
+    }
+  }
 
   return {
     factor_0 : factor_0,
--- test/largest_palindrome_product-spec.js
+++ test/largest_palindrome_product-spec.js
@@ -1,7 +1,7 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-var findPalidromeProduct = require("../largest_palindrome_product.js");
+var findPalindromeProduct = require("../largest_palindrome_product.js");
 
 describe("The Largest Palindrome Product", function () {
 
@@ -9,13 +9,13 @@ describe("The Largest Palindrome Product", function () {
     var result = findPalindromeProduct(2,2);
     expect(result.palindromeNumber).to.equal(9009);
     // make sure solution contains the two factors
-    expect([91, 99]).to.have.members(result.factor_0);
-    expect([91, 99]).to.have.members(result.factor_1);
+    expect([91, 99]).to.include(result.factor_0);
+    expect([91, 99]).to.include(result.factor_1);
   });
 
   it("of two 3 digit numbers should be 906609", function () {
-    var product = challenge_4.largestPalindromeProduct(2,3);
-    expect(product.palindromeNumber).toEqual(906609);
+    var product = findPalindromeProduct(2,3);
+    expect(product.palindromeNumber).to.equal(906609);
   });
 
 });
